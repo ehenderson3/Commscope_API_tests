64d116e0aaac443e58f2ceb9944c9c59d64d3241
DESKTOP-Q473760\ehend <ehenderson@surgeforward.com>
2017-03-11 10:31:50 -0700

    git fetch && git checkout COM-172-PUT_ProjectSettings

--- src/main/java/comm/service/model/Projects.java
+++ src/main/java/comm/service/model/Projects.java
@@ -9,6 +9,17 @@ public class Projects {
     private String projectName;
     private String projectType;
 
+
+
+    private String defaultLicensee;
+    private int fresnelZoneRadius;
+    private double kFactor;
+    private String minimumClearance;
+    private boolean showSiteLocationDetails;
+    private double targetAvailability;
+    private String unitType;
+
+
     public Projects(){};
 
     public Projects(int createUserId, int defaultLicensee, String projectName, String projectType){
@@ -18,10 +29,77 @@ public class Projects {
         this.projectType = projectType;
     };
 
-    public Projects(String projectName, String projectType){
+    public Projects(String projectName, int fresnelZoneRadius, double kFactor, int i, boolean showSiteLocationDetails, double targetAvailability, String projectType){
         this.projectName = projectName;
         this.projectType = projectType;
     };
+
+
+    public Projects(String defaultLicenseeVal, int fresnelZoneRadiusVal, double kFactorVal, String minimumClearanceVal, boolean showSiteLocationDetailsVal,double targetAvailabilityVal, String unitTypeVal ){
+        this.defaultLicensee = defaultLicenseeVal;
+        this.fresnelZoneRadius = fresnelZoneRadiusVal;
+        this.kFactor = kFactorVal;
+        this.minimumClearance = minimumClearanceVal;
+        this.showSiteLocationDetails = showSiteLocationDetailsVal;
+        this.targetAvailability = targetAvailabilityVal;
+        this.unitType = unitTypeVal;
+    };
+
+    public Projects(int fresnelZoneRadiusVal ){
+        this.fresnelZoneRadius = fresnelZoneRadiusVal;
+    };
+
+    public Projects(String unitTypeVal ){
+        this.unitType = unitTypeVal;
+    };
+
+    public String getDefaultLicensee(){
+        return defaultLicensee;
+    }
+    public void setDefaultLicensee(String defaultLicenseeVal){
+        this.defaultLicensee = defaultLicenseeVal;
+    }
+
+    public int getFresnelZoneRadius(){
+        return fresnelZoneRadius;
+    }
+    public void setFresnelZoneRadius(int fresnelZoneRadiusVal){
+        this.fresnelZoneRadius = fresnelZoneRadiusVal;
+    }
+
+    public double getKFactor(){
+        return kFactor;
+    }
+    public void setKFactor(double kFactorVal){
+        this.kFactor = kFactorVal;
+    }
+
+    public String getMinimumClearance(){
+        return minimumClearance;
+    }
+    public void setMinimumClearance(String minimumClearanceVal){
+        this.minimumClearance = minimumClearanceVal;
+    }
+
+    public boolean getShowSiteLocationDetails(){
+        return showSiteLocationDetails;
+    }
+    public void setShowSiteLocationDetails(boolean showSiteLocationDetailsVal){
+        this.showSiteLocationDetails = showSiteLocationDetailsVal;}
+
+    public double getTargetAvailability(){
+        return targetAvailability;
+    }
+    public void setTargetAvailability(double targetAvailabilityVal){
+        this.targetAvailability = targetAvailabilityVal;}
+
+    public String getUnitType(){
+        return unitType;
+    }
+    public void setUnitType(String unitTypeVal) {
+        this.unitType = unitTypeVal;
+
+    }
 
 
     public int getCreateUserId(){
