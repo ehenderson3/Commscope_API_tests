821daf201b4ccb433f5f6dc43d8cbc66050a2407
DESKTOP-Q473760\ehend <ehenderson@surgeforward.com>
2017-03-21 16:17:08 -0700

    adding common/Data

---
+++ src/test/java/comm/service/tests/common/Data.java
@@ -0,0 +1,20 @@
+package comm.service.tests.common;
+
+/**
+ * Created by ehend on 3/16/2017.
+ */
+public class Data {
+    public static final String ERROR_MIN_CLEARANCE_OUT_OF_RANGE = "Minimum Clearance is outside the range value";
+    public static final String ERROR_FRESEL_ZONE_OUT_OF_RANGE = "fresnel zone radius is outside the range value";
+    public static final String ERROR_TARGET_AVAILABILITY_OUT_OF_RANGE = "Target Availability is outside the range value";
+    public static final String ERROR_K_FACTOR_OUT_OF_RANGE = "K Factor is outside the range value";
+    public static final String ERROR_CANNOT_DESERIALIZE_STRING = "Could not read document: Can not deserialize value of type java.lang.Integer from String ";
+    public static final String ERROR_CANNOT_DESERIALIZE_FLOAT = "Could not read document: Can not deserialize value of type java.lang.Float from String ";
+
+    public static final String ERROR_CANNOT_DESERIALIZE_INT = "Could not read document: Can not deserialize value of type java.lang.Integer from String ";
+    public static final String PROJECT_SUCCESS_MESSAGE = "Successfully updated Project";
+
+
+
+
+}
